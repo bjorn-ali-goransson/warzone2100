7b53b946cb5deaf0e376755229825d352ed948b5
bjorn-ali-goransson <bjorn.a.goransson@gmail.com>
2020-09-20 11:49:48 +0200

    Update

--- src/flowfield.h
+++ src/flowfield.h
@@ -39,56 +39,41 @@
 // TODO: support marking sectors as dirty: portals needs to be rebuild, cost fields recalculated, and integration and flow fields for that sector must be removed from cache.
 
 // TODO: disable before merge with main branch
-#define I_AM_FLOW_FIELD_MAINTAINER_AND_WANT_TO_HAVE_MY_VIEW_OBSCURED 1
+constexpr const bool DEBUG_BUILD = true;
 
-#if defined(NDEBUG) || !defined(I_AM_FLOW_FIELD_MAINTAINER_AND_WANT_TO_HAVE_MY_VIEW_OBSCURED)
-	// General debug flag for things not related to other debug flags declared below
-	constexpr const bool DEBUG_BUILD = false;
+// FEEL FREE TO DISABLE THOSE AS THEY OBSCURE THE VIEW
+constexpr const bool COST_FIELD_DEBUG = true;
+constexpr const bool PORTALS_DEBUG = true;
+constexpr const bool PORTAL_PATH_DEBUG = true;
+constexpr const bool VECTOR_FIELD_DEBUG = true;
+constexpr const bool DEBUG_A_STAR = true;
+constexpr const bool DEBUG_THREAD_POOL = true;
 
-	constexpr const bool COST_FIELD_DEBUG = false;
-	constexpr const bool PORTALS_DEBUG = false;
-	constexpr const bool PORTAL_PATH_DEBUG = false;
-	constexpr const bool VECTOR_FIELD_DEBUG = false;
-	constexpr const bool DEBUG_A_STAR = false;
-	constexpr const bool DEBUG_THREAD_POOL = false;
-#else
-	// General debug flag for things not related to other debug flags declared below
-	constexpr const bool DEBUG_BUILD = true;
+void flowfieldEnable();
+bool isFlowfieldEnabled();
 
-	// FEEL FREE TO DISABLE THOSE AS THEY OBSCURE THE VIEW
-	constexpr const bool COST_FIELD_DEBUG = true;
-	constexpr const bool PORTALS_DEBUG = true;
-	constexpr const bool PORTAL_PATH_DEBUG = true;
-	constexpr const bool VECTOR_FIELD_DEBUG = true;
-	constexpr const bool DEBUG_A_STAR = true;
-	constexpr const bool DEBUG_THREAD_POOL = true;
-#endif
+void flowfieldInit();
+void flowfieldDestroy();
 
-	void flowfieldEnable();
-	bool isFlowfieldEnabled();
+/**
+ * Public interface same as fpathRoute for compatibility.
+ * Most fields are not yet used in current flowfield implementation, but I wanted it to be compatible with fpath.cpp
+ */
+void calculateFlowfieldsAsync(MOVE_CONTROL *psMove, unsigned id, int startX, int startY, int tX, int tY, PROPULSION_TYPE propulsionType,
+								DROID_TYPE droidType, FPATH_MOVETYPE moveType, int owner, bool acceptNearest, StructureBounds const &dstStructure);
 
-	void flowfieldInit();
-	void flowfieldDestroy();
+std::deque<unsigned int> getFlowfieldPathFromCache(unsigned startX, unsigned startY, unsigned tX, unsigned tY, PROPULSION_TYPE propulsion);
 
-	/**
-	 * Public interface same as fpathRoute for compatibility.
-	 * Most fields are not yet used in current flowfield implementation, but I wanted it to be compatible with fpath.cpp
-	 */
-	void calculateFlowfieldsAsync(MOVE_CONTROL *psMove, unsigned id, int startX, int startY, int tX, int tY, PROPULSION_TYPE propulsionType,
-								  DROID_TYPE droidType, FPATH_MOVETYPE moveType, int owner, bool acceptNearest, StructureBounds const &dstStructure);
+Vector2f getFlowfieldMovementVector(unsigned int nextPortalId, unsigned currentX, unsigned currentY, PROPULSION_TYPE propulsion);
 
-	std::deque<unsigned int> getFlowfieldPathFromCache(unsigned startX, unsigned startY, unsigned tX, unsigned tY, PROPULSION_TYPE propulsion);
+std::vector<Vector2i> flowfieldPortalPathToCoordsPath(const std::deque<unsigned int>& path, DROID* psDroid);
 
-	Vector2f getFlowfieldMovementVector(unsigned int nextPortalId, unsigned currentX, unsigned currentY, PROPULSION_TYPE propulsion);
+void debugDrawFlowfields(const glm::mat4 &mvp);
 
-	std::vector<Vector2i> flowfieldPortalPathToCoordsPath(const std::deque<unsigned int>& path, DROID* psDroid);
+/** Initialise the path-finding module.
+ */
+bool ffpathInitialise();
 
-	void debugDrawFlowfields(const glm::mat4 &mvp);
-
-	/** Initialise the path-finding module.
-	 */
-	bool ffpathInitialise();
-
-	/** Shutdown the path-finding module.
-	 */
-	void ffpathShutdown();
+/** Shutdown the path-finding module.
+ */
+void ffpathShutdown();
